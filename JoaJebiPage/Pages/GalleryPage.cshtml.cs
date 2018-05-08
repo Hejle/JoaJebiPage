a0fd5ef6a9399c3e295a8b671b83d80b2438dda0
Hejle <johej15@student.sdu.dk>
2018-05-08 13:30:04 +0200

    Added GalleryPage that shows images

---
+++ JoaJebiPage/Pages/GalleryPage.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JoaJebiPage.Pages
+{
+    public class GalleryPageModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public string[] GetImages()
+        {
+            string path = "wwwroot/images/galary";
+            var files = Directory.GetFiles(path);
+            var images = new List<string>();
+            foreach (var file in files)
+            {
+                string s = file.Replace("\\", "/").Replace("wwwroot", "");
+                images.Add(s);
+            }
+            
+            return images.ToArray();
+        }
+    }
+}

5b21f891cb97a71cf6f073477aaa7b6ab7a00470
Hejle <johej15@student.sdu.dk>
2018-05-12 14:13:52 +0200

    Added method to upload text to the server in string format. The uploaded string is formatted using HTML and is not checked for crossscripting

---
+++ JoaJebiPage/Pages/AdminPage.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JoaJebiPage.Pages
+{
+    public class AdminPageModel : PageModel
+    {
+
+        [Required]
+        [BindProperty]
+        public string TextUpload { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task OnPostAsync()
+        {
+            if (TextUpload != null)
+            {
+                
+            }
+        }
+    }
+}

bedf1bf1a20edb0cea7585720fa8a283711a7f11
Hejle <johej15@student.sdu.dk>
2018-05-14 16:33:43 +0200

    Added Selection in About Me Page

--- JoaJebiPage/Pages/About.cshtml.cs
+++ JoaJebiPage/Pages/About.cshtml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using JoaJebiPage.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace JoaJebiPage.Pages
@@ -14,27 +16,41 @@ namespace JoaJebiPage.Pages
 		public string Birthday { get; set; }
 		public string Description { get; set; }
 		public string Picture { get; set; }
-		public List<string> serialNumbers;
+        [BindProperty]
+	    public PersonEnum.Person Who { get; set; }
+        public List<string> serialNumbers;
 
-		public void OnGet()
-		{         
+	    public void OnGet()
+	    {
+	        LoadData(PersonEnum.Person.Jebisan);
+	    }
 
-			string information;
-            using (StreamReader reader = new StreamReader("Jebisan.txt"))
-            {
-				information = reader.ReadLine();
-            }
-                     
-            string[] splittedWords = information.Split(new Char[] {','});
+	    private void LoadData(PersonEnum.Person person)
+	    {
+	        string information;
+	        using (StreamReader reader = new StreamReader(person + ".txt"))
+	        {
+	            information = reader.ReadLine();
+	        }
 
-			FirstName = splittedWords[0];
-			LastName = splittedWords[1];
-			Birthday = splittedWords[2];
-			Description = splittedWords[3];
-			Picture = splittedWords[4];         
+	        string[] splittedWords = information.Split(new Char[] { ',' });
 
-		}
+	        FirstName = splittedWords[0];
+	        LastName = splittedWords[1];
+	        Birthday = splittedWords[2];
+	        Description = splittedWords[3];
+	        Picture = splittedWords[4];
+        }
 
+	    public PersonEnum.Person[] GetPersons()
+	    {
+	        PersonEnum.Person[] plist = { PersonEnum.Person.Jebisan, PersonEnum.Person.Joachim };
+	        return plist;
+	    }
 
-	}
+	    public void OnPostAboutAsync()
+	    {
+	        LoadData(Who);
+	    }
+    }
 }
